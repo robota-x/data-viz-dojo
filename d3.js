1fe8264ca254dab4c202311b839cd64cdc557f38
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-02-09 14:53:37 +0000

    task 2

--- d3.js
+++ d3.js
@@ -1,6 +1,22 @@
 $(document).ready(() => {
+  const cont = d3.select("#my-container");
 
-    d3.select("#my-container")
+  cont.append("div").attr("id", "not-red-square");
+
+  cont
+    .data([1])
+    .enter()
     .append("div")
-    .attr("id", 'not-red-square');
-  });
+    .attr("id", "not-red-square");
+
+  cont
+    .data([...Array(100).keys()])
+    .enter()
+    .append("div")
+    .attr("class", "red-square")
+    .text(n => n)
+    .style({
+      left: n => `${(n % 10 + 1) * 34 + 100}px`,
+      top: n => `${(Math.floor(n / 10) + 1) * 34 + 100}px`
+    });
+});
